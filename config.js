6d6a792e959a6b98467ae113e38f3c0cf4917b61
phuocdaohuu88 <91676904+phuocdaohuu88@users.noreply.github.com>
2021-09-30 21:13:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    titleWeb: "crush",
+    introTitle: 'hé luu crush',
+    introDesc: `mình có điều này bạn phải nói thật lòng nha😘`,
+    btnIntro: '^^okii luôn^^',
+    title: 'bạn thích mình có phải không🥰',
+    desc: 'nếu mà bạn thoát ra thì bạn là người yêu mình đó nha😁',
+    btnYes: 'đúng rồi thích Phước đấy❤️',
+    btnNo: 'Không nha, không có chuyện đó đâu :3',
+    question: 'lí do bạn thích mình là gì thế❤️',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'tại vì thích là thích chẳng cần lí do',
+    mess: 'Mình biết mà 🥰. iuiu nhìu 😘😘',
+    messDesc: 'vậy chúng mình bật chế độ hẹn hò nhá.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
